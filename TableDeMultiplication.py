3a9501c51b75fae5888ad59d954f132f1e20aa27
abdessamadamchar24 <abdessamadamchar24@gmail.com>
2025-10-20 19:26:30 +0100

    Add files via upload

---
+++ TableDeMultiplication.py
@@ -0,0 +1,11 @@
+file = open("TableDeMultiplication.txt","w+")
+
+for i in range(1,10):
+    file.write(f"La table de multiplication de {i} :\n")
+    for j in range(1,10):
+        file.write(f"{i} x {j} = {i*j}\n")
+
+    file.write("\n\n")
+
+
+file.close()
